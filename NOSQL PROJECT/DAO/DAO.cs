d438fc3fdb88116caa23b05f0eb80b8dae9ec5a5
tsxgli <682624@student.inholland.nl>
2022-10-12 12:57:56 +0200

    did forms and model class

---
+++ NOSQL PROJECT/DAO/DAO.cs
@@ -0,0 +1,18 @@
+﻿using MODEL;
+using MongoDB.Driver;
+using System;
+
+namespace DAO
+{
+    public class Dao
+    {
+        static MongoClient client = new MongoClient();
+        static IMongoDatabase db = client.GetDatabase("Project_2_1_NOSQL");
+
+        //get ticket collection
+        static IMongoCollection<Ticket> ticketCollection = db.GetCollection<Ticket>("Tickets");
+
+        //get employee collection
+        static IMongoCollection<Employee> employeeCollection = db.GetCollection<Employee>("Employees");
+    }
+}
