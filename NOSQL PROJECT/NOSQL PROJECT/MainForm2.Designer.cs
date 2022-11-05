3cdad3fbcf76b1c4d7cb6744799456f28cdd39a5
RaresSimion <683655@student.inholland.nl>
2022-11-05 12:10:34 +0100

    idk

--- NOSQL PROJECT/NOSQL PROJECT/MainForm2.Designer.cs
+++ NOSQL PROJECT/NOSQL PROJECT/MainForm2.Designer.cs
@@ -327,7 +327,7 @@
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(890, 463);
             this.panel1.TabIndex = 0;
-            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            //this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
             // comboLocation
             // 
