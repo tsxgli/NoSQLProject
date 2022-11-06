cbbc644a16fe3cfc525575aeebff3c7883861dbd
tsxgli <682624@student.inholland.nl>
2022-11-06 19:54:46 +0100

    doing adduser

--- NOSQL PROJECT/NOSQL PROJECT/MainForm2.cs
+++ NOSQL PROJECT/NOSQL PROJECT/MainForm2.cs
@@ -54,11 +54,26 @@ namespace NOSQL_PROJECT
             ticket.Deadline = dtp_Deadline.Value;
             ticket.Description = txt_IncidentDescription.Text;
             ticket.ReportedDate = dtPick_IncidentTimeReported.Value;
-            //int index = comb_ReportedByUser.SelectedIndex;   
-            ticket.UserReported = employees[comb_ReportedByUser.SelectedIndex];
+            switch (comb_TypeIncident.GetItemText(comb_TypeIncident.SelectedItem))
+            {
+                case "Hardware":
+                    ticket.TicketType = TicketType.Hardware;
+                    break;
+                case "Software":
+                    ticket.TicketType = TicketType.Software;
+                    break;
+                case "Service":
+                    ticket.TicketType = TicketType.Service;
+                    break;
+                default :
+                    ticket.TicketType = TicketType.Service;
+                    break;
+            }
 
+            ticket.UserReported = employees[comb_ReportedByUser.SelectedIndex];
             ticket.TicketStatus = TicketStatus.Open;
 
+            //add ticket to database
             incidentLogic.AddNewIncident(ticket);
         }
         public void PopulateComboBox()
@@ -82,7 +97,50 @@ namespace NOSQL_PROJECT
         private void btnSubmitTicket_Click(object sender, EventArgs e)
         {
             AddIncidentToDB();
-            MessageBox.Show("Incident Created");
+            MessageBox.Show("Incident has been succesfully created.");
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void AddEmployeeToDatabase()
+        {
+            incidentLogic = new IncidentLogic();
+
+            Employee employee= new Employee();
+
+            employee.FirstName =txtFirstName.Text;
+            employee.LastName =txtLastName.Text;
+            employee.Email =txtEmail.Text;
+            employee.Location = comboLocation.SelectedText;
+            employee.PhoneNumber=txtPhoneNo.Text;
+
+
+            switch (comboUserType.GetItemText(comboUserType.SelectedItem))
+            {
+                case "Service Desk":
+                    employee.UserType = UserType.ServiceDesk;
+                    break;
+                case "Regular":
+                    employee.UserType = UserType.Regular;
+                    break;
+                default:
+                    employee.UserType = UserType.Regular;
+                    break;
+            }
+
+            if (checkBoxSendEmail.Checked)
+            {
+
+            }
+            
+        }
+
+        public void SendPasswordToEmail()
+        {
+
         }
 
     }
